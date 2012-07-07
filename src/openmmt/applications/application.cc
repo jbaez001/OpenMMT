14ac9d0b8d5ee6e64e0f687a8d9771f92e16e813
Juan Baez <jbaez001@gmail.com>
2012-07-07 14:29:40 -0500

    Updated copyright notices.
    Copyright (C) 2010-2012 Genscripts

--- src/openmmt/applications/application.cc
+++ src/openmmt/applications/application.cc
@@ -1,6 +1,6 @@
 /*************************************************************************
  * OpenMMT - Open Multi-Monitor Taskbar
- * Copyright (C) 2010-2011 Genscripts
+ * Copyright (C) 2010-2012 Genscripts
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
